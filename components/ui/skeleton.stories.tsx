e442327b4c62267df112dc6af8883754675c603d
Alcides Rodriguez <alcidesrodriguezreyes@gmail.com>
2024-05-31 12:38:05 -0500

    skeleton

---
+++ components/ui/skeleton.stories.tsx
@@ -0,0 +1,45 @@
+// create storybook stories for skeleton component
+
+import { Meta, StoryObj } from '@storybook/react';
+import { Skeleton } from './skeleton';
+
+type Story = StoryObj<typeof Skeleton>;
+
+const meta: Meta<typeof Skeleton> = {
+    title: 'UI/Skeleton',
+    component: Skeleton,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+export const Default: Story = {
+    render: () => {
+        return (
+            <div className="flex items-center space-x-4">
+                <Skeleton className="h-12 w-12 rounded-full" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div>
+        );
+    },
+};
+
+export const Card: Story = {
+    render: () => {
+        return (
+            <div className="flex flex-col space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div>
+        );
+    },
+};
+
+export default meta;
